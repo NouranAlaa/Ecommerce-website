9f1aef7d90bf829b663f1d19591ad18235582b15
NouranAlaa <nouranougazy@gmail.com>
2020-10-03 03:34:36 +0200

    Adding media directory

--- ecommerce_website/urls.py
+++ ecommerce_website/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views
+from django.conf import settings
+from django.conf.urls.static import static
 from shop import views
+from blog import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index,name='Home'),
     path('blog/', include("blog.urls")),
     path('shop/', include("shop.urls")),
-]
+
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
